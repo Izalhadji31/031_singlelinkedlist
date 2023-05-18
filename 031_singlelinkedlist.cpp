01d1bed485b2b477cbcb4b96df67b7a204d453e3
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-18 19:10:25 +0700

    membuat int main

--- 031_singlelinkedlist.cpp
+++ 031_singlelinkedlist.cpp
@@ -117,3 +117,16 @@ void searchData() {
 		cout << "Data tidak ditemukan" << endl;
 	}
 }
+int main() {
+	int pilihan;
+	do {
+		try {
+
+			cout << "1. Menambahkan Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tanpilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+
+			cout << "Pilihan : ";
+			cin >> pilihan;
